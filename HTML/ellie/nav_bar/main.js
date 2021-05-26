bcc690ce12936f379f72ba55d97b867ad3041bb2
unknown <90manbo@gmail.com>
2021-05-26 16:13:43 +0900

    Update

--- HTML/ellie/nav_bar/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar_toggleBtn');
-const list = document.querySelector('.navbar_list');
-const sns = document.querySelector('.navbar_sns');
-
-toggleBtn.addEventListener('click', ()=>{
-    list.classList.toggle('active');
-    sns.classList.toggle('active');
-});
