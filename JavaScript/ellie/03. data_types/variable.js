93dce0b606433b75584027e01d359625a7d73995
unknown <90manbo@gmail.com>
2021-07-08 10:37:48 +0900

    Update

--- JavaScript/ellie/03. data_types/variable.js
+++ JavaScript/ellie/03. data_types/variable.js
@@ -5,7 +5,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// 2. Variable
+// 2. Variable (read and write)
 // mutable
 // let (added in ES6)
 
@@ -27,20 +27,27 @@ console.log(name);
 
 // 3. var(don't ever use this)
 // var hoisting(move declaration form bottom to top)
-//어디에 선언했는지에 상관없이 선언을 제일 위로 끌어 올림.
-//block scope를 사용할 수 없음. 
+// 어디에 선언했는지에 상관없이 선언을 제일 위로 끌어 올림.
+// block scope를 사용할 수 없음. 
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// 4. constants
-//favor immutable data type always for a few resons:
-// - security
-// - thread safety
-// - reduce human mistakes
+// 4. constant(read only)
+// use const whenever possible.
+// only use let if variable needs to change
+
 const daysInWeek = 7;
 const maxNumber = 5;
 
 //////////////////////////////////////////////////////////////////////////////////
+
+// Note!
+// Immutable data types: primitive types, frozen objects(i.e. object.freeze())
+// Mutabale data types: all objects by default are mutable in JS
+// favor immutable data type always for a few resons:
+// - security
+// - thread safety
+// - reduce human mistakes
 
 // 5. Variable types
 // primitive type, single item: number, string, boolean, null, undefined, symbol
---
+++ JavaScript/ellie/04. operator_loop/operator2.js
@@ -0,0 +1,59 @@
+// 8. Conditional operators : if
+// if, else if, else
+const name = 'ellie';
+if (name === 'ellie') {
+    console.log('ellie');
+}else if (name === 'coder'){
+    console.log('coder');
+}else {
+    console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'ellie' ? 'yes' : 'no');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'IE';
+switch (browser) {
+    case 'IE' :
+        console.log('go away');
+        break;
+    case 'Chrome' :
+        case 'Firefox' :
+        console.log('love you');
+        break;
+    default:
+        console.log('same all');
+        break;
+    
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while ( i > 0){
+    console.log(`while: ${i}`);
+    i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition
+do {
+    console.log(`do while: ${i}`);
+    i--;
+}while (i > 0);
+
+// for loop, for(begin; condition; step)
+for(i = 3; i> 0; i--){
+    console.log(`for: ${i}`);
+}
+
+for(let i =3; i> 0; i= i-2){
+    //inline variable declaration
+    console.log(`inline variable for: ${i}`);
+}
