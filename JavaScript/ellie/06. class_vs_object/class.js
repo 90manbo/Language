0a037043822bb25d5c0c5331ece5a21b7c6db86c
unknown <90manbo@gmail.com>
2021-07-12 14:38:54 +0900

    Update

--- JavaScript/ellie/06. class_vs_object/class.js
+++ JavaScript/ellie/06. class_vs_object/class.js
@@ -111,6 +111,10 @@ class Triangle extends Shape{
     getArea() {
         return (this.width * this.height) / 2;
     }
+
+    toString() {
+        return `Triangle: color ${this.color}`;
+    }
 }
 
 const rectangle = new Rectangle(20, 20, 'blue');
@@ -127,3 +131,4 @@ console.log(triangle instanceof Rectangle);
 console.log(triangle instanceof Triangle);
 console.log(triangle instanceof Shape);
 console.log(triangle instanceof Object);
+console.log(triangle.toString());
